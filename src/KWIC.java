f51103414699a6833bfc6130b9f7c1a351c01f36
BlitJian <a0088326@nus.edu.sg>
2014-08-28 21:24:32 +0800

    Uppercasing keyword and lowercasing filter
    test data:
    the day after tomorrow
    fast and furious
    man of
    steel
    
    is
    the
    of
    and
    as
    a
    after

--- src/KWIC.java
+++ src/KWIC.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
@@ -12,11 +13,16 @@ public class KWIC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 
-		String filters[] = {"is", "the", "of", "and", "as", "a", "after"};
+		List<String> filters = new ArrayList<String>();//Arrays.asList("is", "the", "of", "and", "as", "a", "after");
 		List<String> outputs = new ArrayList<String>();
+		System.out.println("Enter title:");
 		outputs = getLines();
+		System.out.println("Enter filter word:");
+		filters = getLines();
+		
 		outputs = rotateString(outputs);
 		outputs = filterFirstWord(outputs, filters);
+		outputs = upperCaseKey(outputs, filters);
 		outputs = sort(outputs);
 		display(outputs);
 	}
@@ -28,7 +34,6 @@ public class KWIC {
 		List<String> lines = new ArrayList<String>();
 		String line = "";
 		while(true){
-			System.out.println("Enter title:");
 			line = reader.nextLine();
 			if(!line.equals(""))
 			{
@@ -39,7 +44,7 @@ public class KWIC {
 				break;
 			}
 		}
-		reader.close();
+//		reader.close();
 		return lines;
 	}
 
@@ -65,9 +70,8 @@ public class KWIC {
 		return outputs;
 	}
 	
-	private static List<String> filterFirstWord(List<String >inputs, String[] filters)
+	private static List<String> filterFirstWord(List<String >inputs, List<String> filters)
 	{
-		List<String> outputs = new ArrayList<String>();
 		for(int i = 0; i<inputs.size();i++)
 		{
 			for(String filter: filters)
@@ -81,21 +85,76 @@ public class KWIC {
 		}
 		return inputs;
 	}
+	
+	private static List<String> upperCaseKey (List<String> inputs, List<String> filters)
+	{
+		List<String> outputs = new ArrayList<String>();
+		for(String input: inputs)
+		{
+			String temp = "";
+			String[] splitInputs = input.split(" ");
+			for(String splitInput: splitInputs)
+			{
+				boolean exist = false;
+				for(String filter: filters)
+				{
+					if(splitInput.equalsIgnoreCase(filter))
+					{
+						exist = true;
+						continue;
+					}
+
+				}
+				if(!exist && splitInput.charAt(0)<='z' && splitInput.charAt(0)>='a')
+				{
+					splitInput = (char)(splitInput.charAt(0)-32)+splitInput.substring(1, splitInput.length());
+				}
+				
+				if(exist && splitInput.charAt(0)<='Z' && splitInput.charAt(0)>='A')
+				{
+					splitInput = (char)(splitInput.charAt(0)+32)+splitInput.substring(1, splitInput.length());					
+				}
+				temp += splitInput + " ";
+			}
+			outputs.add(temp.substring(0, temp.length()-1));
+		}
+		return outputs;		
+	}
 	private static List<String> sort(List<String> inputs)
 	{
 		 Comparator<String> cmp = new Comparator<String>(){
 			 public int compare(String s1, String s2){
 				 int s1char = s1.charAt(0), s2char = s2.charAt(0);
-//				 System.out.println((char)(s2.charAt(0)-32));
+				 				 
 				 if(s1.charAt(0)<='z' && s1.charAt(0)>='a')
 					 s1char = s1.charAt(0) - 32;
 
 				 if(s2.charAt(0)<='z' && s2.charAt(0)>='a')
 					 s2char = s2.charAt(0) - 32;
+
 				 if(s1char!=s2char)
+				 {
 					 return s1char-s2char;
+				 }
 				 else
-					 return compare(s1.substring(1, s1.length()-1), s2.substring(1, s2.length()-1));
+				 {
+					 //s1 only had last character to check
+					 if(s1.length()==1)
+					 {
+						 if(s2.length()>1)
+							 return -1;
+					 }
+					 //s2 only had last character to check					 
+					 if(s2.length()==1)
+					 {
+						 //All characters are the same
+						 if(s1.equalsIgnoreCase(s2))
+							 return 0;
+						 else
+							 return 1;
+					 }
+					 return compare(s1.substring(1, s1.length()), s2.substring(1, s2.length()));
+				 }
 			 }};
 			 inputs.sort(cmp);
 		return inputs;
@@ -106,7 +165,7 @@ public class KWIC {
 		for(String input: inputs)
 		{
 			boolean moreThanOneWord = input.contains(" ");
-			System.out.println("["+input.substring(0, (moreThanOneWord? input.indexOf(" "):input.length()))+"]"+(moreThanOneWord? input.substring(input.indexOf(" "), input.length()-1): ""));
+			System.out.println("["+input.substring(0, (moreThanOneWord? input.indexOf(" "):input.length()))+"]"+(moreThanOneWord? input.substring(input.indexOf(" "), input.length()): ""));
 		}
 	}
 	
